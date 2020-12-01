45a0d6f05eeddac2e918cbff027d4b3ec5f83e0c
shashikala anil <shashikala.ghaste@gmail.com>
2020-12-01 15:32:32 +0530

    added platfomr

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
+var platform;
 
 function setup(){
     var canvas = createCanvas(1200,400);
@@ -12,6 +13,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform=new Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -52,6 +54,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    platform.display();
 
     bird.display();
 }
